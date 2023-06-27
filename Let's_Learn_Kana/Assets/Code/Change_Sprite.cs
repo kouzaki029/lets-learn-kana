6845661844fb115a571c9622925642e03e298a8b
kouzaki029 <122118874+kouzaki029@users.noreply.github.com>
2023-06-27 15:43:54 -0400

    added sprites and some code

---
+++ Let's_Learn_Kana/Assets/Code/Change_Sprite.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Change_Sprite : MonoBehaviour
+{
+// class variables
+    public SpriteRenderer oldSprite;
+    public Sprite newSprite;
+
+// class methods
+    void ChangeSprite()
+    {
+        oldSprite.sprite = newSprite;
+    }
+    void Start() // Start is called before the first frame update
+    {}   
+    void Update() // Update is called once per frame
+    {}
+}
