05907e67d5431df2c4f5779757374a1870c8361f
kouzaki029 <122118874+kouzaki029@users.noreply.github.com>
2023-07-18 01:11:57 -0400

    .

--- Let's_Learn_Kana/Assets/Code/Drag_and_Drop.cs
+++ Let's_Learn_Kana/Assets/Code/Drag_and_Drop.cs
@@ -46,7 +46,34 @@ public class Drag_and_Drop : MonoBehaviour
         Vector3 originalSoundPosition05 = new(6.98f, -2.41f, 0.00f);
 
         CheckAnswer(soundPosition01, kanaPosition01, originalSoundPosition01, originalKanaPosition01);
+        CheckAnswer(soundPosition01, kanaPosition02, originalSoundPosition01, originalKanaPosition02);
+        CheckAnswer(soundPosition01, kanaPosition03, originalSoundPosition01, originalKanaPosition03);
+        CheckAnswer(soundPosition01, kanaPosition04, originalSoundPosition01, originalKanaPosition04);
+        CheckAnswer(soundPosition01, kanaPosition05, originalSoundPosition01, originalKanaPosition05);
 
+        CheckAnswer(soundPosition02, kanaPosition01, originalSoundPosition02, originalKanaPosition01);
+        CheckAnswer(soundPosition02, kanaPosition02, originalSoundPosition02, originalKanaPosition02);
+        CheckAnswer(soundPosition02, kanaPosition03, originalSoundPosition02, originalKanaPosition03);
+        CheckAnswer(soundPosition02, kanaPosition04, originalSoundPosition02, originalKanaPosition04);
+        CheckAnswer(soundPosition02, kanaPosition05, originalSoundPosition02, originalKanaPosition05);
+
+        CheckAnswer(soundPosition03, kanaPosition01, originalSoundPosition03, originalKanaPosition01);
+        CheckAnswer(soundPosition03, kanaPosition02, originalSoundPosition03, originalKanaPosition02);
+        CheckAnswer(soundPosition03, kanaPosition03, originalSoundPosition03, originalKanaPosition03);
+        CheckAnswer(soundPosition03, kanaPosition04, originalSoundPosition03, originalKanaPosition04);
+        CheckAnswer(soundPosition03, kanaPosition05, originalSoundPosition03, originalKanaPosition05);
+
+        CheckAnswer(soundPosition04, kanaPosition01, originalSoundPosition04, originalKanaPosition01);
+        CheckAnswer(soundPosition04, kanaPosition02, originalSoundPosition04, originalKanaPosition02);
+        CheckAnswer(soundPosition04, kanaPosition03, originalSoundPosition04, originalKanaPosition03);
+        CheckAnswer(soundPosition04, kanaPosition04, originalSoundPosition04, originalKanaPosition04);
+        CheckAnswer(soundPosition04, kanaPosition05, originalSoundPosition04, originalKanaPosition05);
+
+        CheckAnswer(soundPosition05, kanaPosition01, originalSoundPosition05, originalKanaPosition01);
+        CheckAnswer(soundPosition05, kanaPosition02, originalSoundPosition05, originalKanaPosition02);
+        CheckAnswer(soundPosition05, kanaPosition03, originalSoundPosition05, originalKanaPosition03);
+        CheckAnswer(soundPosition05, kanaPosition04, originalSoundPosition05, originalKanaPosition04);
+        CheckAnswer(soundPosition05, kanaPosition05, originalSoundPosition05, originalKanaPosition05);
     }
     public void CheckAnswer(GameObject firstSprite, GameObject secondSprite, Vector3 originalLocation01, Vector3 originalLocation02)
     {
