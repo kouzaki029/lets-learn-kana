909a843c63a3c404c4a9e80dcbbe9b395f1b8a57
kouzaki027 <122118874+kouzaki029@users.noreply.github.com>
2024-01-02 20:19:33 -0500

    Noticed Bug, and also Removing Elements from List
    1) Noticed a bug where the last correctly guessed answer is generated again during the next round
    
    2) Started to make it where the sprites and sounds that were all ready spawned, cannot be spawned again

--- Let's_Learn_Kana/Assets/Code/Drag_and_Drop.cs
+++ Let's_Learn_Kana/Assets/Code/Drag_and_Drop.cs
@@ -5,20 +5,20 @@ using UnityEngine;
 public class Drag_and_Drop : MonoBehaviour
 {
 //class variables
-    private Vector3 dragOffset;
-    private Camera cam;
-    public GameObject kanaPosition01; //default kana location #1
-    public GameObject kanaPosition02; //default kana location #2
-    public GameObject kanaPosition03; //default kana location #3
-    public GameObject kanaPosition04; //default kana location #4
-    public GameObject kanaPosition05; //default kana location #5
-    public GameObject soundPosition01; //default sound location #1
-    public GameObject soundPosition02; //default sound location #2
-    public GameObject soundPosition03; //default sound location #3
-    public GameObject soundPosition04; //default sound location #4
-    public GameObject soundPosition05; //default sound location #5
+    private Vector3     dragOffset;
+    private Camera      cam;
+    public  GameObject  kanaPosition01; //default kana location #1
+    public  GameObject  kanaPosition02; //default kana location #2
+    public  GameObject  kanaPosition03; //default kana location #3
+    public  GameObject  kanaPosition04; //default kana location #4
+    public  GameObject  kanaPosition05; //default kana location #5
+    public  GameObject  soundPosition01; //default sound location #1
+    public  GameObject  soundPosition02; //default sound location #2
+    public  GameObject  soundPosition03; //default sound location #3
+    public  GameObject  soundPosition04; //default sound location #4
+    public  GameObject  soundPosition05; //default sound location #5
 
-    //class methods
+//class methods
     void Awake()
     {
         cam = Camera.main;
@@ -34,11 +34,11 @@ public class Drag_and_Drop : MonoBehaviour
     void OnMouseUp()
     {
         //remembering where the sprites originally used to be
-        Vector3 originalKanaPosition01 = new(-6.98f, 2.41f, 0.00f);
-        Vector3 originalKanaPosition02 = new(-3.60f, 2.41f, 0.00f);
-        Vector3 originalKanaPosition03 = new(0.00f, 2.41f, 0.00f);
-        Vector3 originalKanaPosition04 = new(3.60f, 2.41f, 0.00f);
-        Vector3 originalKanaPosition05 = new(6.98f, 2.41f, 0.00f);
+        Vector3 originalKanaPosition01  = new(-6.98f, 2.41f, 0.00f);
+        Vector3 originalKanaPosition02  = new(-3.60f, 2.41f, 0.00f);
+        Vector3 originalKanaPosition03  = new(0.00f, 2.41f, 0.00f);
+        Vector3 originalKanaPosition04  = new(3.60f, 2.41f, 0.00f);
+        Vector3 originalKanaPosition05  = new(6.98f, 2.41f, 0.00f);
         Vector3 originalSoundPosition01 = new(-6.98f, -2.41f, 0.00f);
         Vector3 originalSoundPosition02 = new(-3.60f, -2.41f, 0.00f);
         Vector3 originalSoundPosition03 = new(0.00f, -2.41f, 0.00f);
@@ -77,17 +77,18 @@ public class Drag_and_Drop : MonoBehaviour
     }
     public void CheckAnswer(GameObject firstSprite, GameObject secondSprite, Vector3 originalLocation01, Vector3 originalLocation02)
     {
-        if (Vector3.Distance(firstSprite.transform.position, secondSprite.transform.position) < .3)
+        if (Vector3.Distance(firstSprite.transform.position, secondSprite.transform.position) < .4)
         {
             if (firstSprite.GetComponent<AudioSource>().clip.name == secondSprite.GetComponent<SpriteRenderer>().sprite.name)
             {
-                firstSprite.SetActive(false); //hides soundPoisition01?
-                secondSprite.SetActive(false);
+                firstSprite.SetActive(false);   //hides the sound sprite
+                secondSprite.SetActive(false);  //hides the kana sprite
+                
             }
             else
             {
-                firstSprite.transform.position = originalLocation01;
-                secondSprite.transform.position = originalLocation02;
+                firstSprite.transform.position = originalLocation01;    //returns the sound sprite to where it was
+                secondSprite.transform.position = originalLocation02;   //returns the kana sprite to where it was
             }
         }
     }
--- Let's_Learn_Kana/Assets/Code/List_Of_Kana.cs
+++ Let's_Learn_Kana/Assets/Code/List_Of_Kana.cs
@@ -5,21 +5,21 @@ using UnityEngine;
 public class List_Of_Kana : MonoBehaviour
 {
 //class variables
-    public GameObject kanaPosition01; //default kana location #1
-    public GameObject kanaPosition02; //default kana location #2
-    public GameObject kanaPosition03; //default kana location #3
-    public GameObject kanaPosition04; //default kana location #4
-    public GameObject kanaPosition05; //default kana location #5
-    public GameObject soundPosition01; //default sound location #1
-    public GameObject soundPosition02; //default sound location #2
-    public GameObject soundPosition03; //default sound location #3
-    public GameObject soundPosition04; //default sound location #4
-    public GameObject soundPosition05; //default sound location #5
-    public List<Sprite> kanaList; //contains all of the kana sprites that are added in Unity, not Visual Studio
-    public List<AudioClip> soundList; //conrains all of the kana sounds that are added in Unity, not Visual Studio
+    public GameObject       kanaPosition01;     //default kana location #1
+    public GameObject       kanaPosition02;     //default kana location #2
+    public GameObject       kanaPosition03;      //default kana location #3
+    public GameObject       kanaPosition04;     //default kana location #4
+    public GameObject       kanaPosition05;     //default kana location #5
+    public GameObject       soundPosition01;    //default sound location #1
+    public GameObject       soundPosition02;    //default sound location #2
+    public GameObject       soundPosition03;    //default sound location #3
+    public GameObject       soundPosition04;    //default sound location #4
+    public GameObject       soundPosition05;    //default sound location #5
+    public List<Sprite>     kanaList;           //contains all of the kana sprites that are added in Unity, not Visual Studio
+    public List<AudioClip>  soundList;          //conrains all of the kana sounds that are added in Unity, not Visual Studio
 
 //class methods
-    public List<int> FiveUniqueRandomNumbers(int maximum) //chooses 5 unique random numbers between 0 and maximum (where maximum is also a possibe random number) 
+    public List<int>    FiveUniqueRandomNumbers(int maximum) //chooses 5 unique random numbers between 0 and maximum (where maximum is also a possibe random number) 
     {
         List<int> fiveRandomNumbers = new List<int>();
 
@@ -33,90 +33,93 @@ public class List_Of_Kana : MonoBehaviour
             if (!(fiveRandomNumbers.Contains(randomNumber))) //sees if the random number is NOT already inside the list
             {
                 fiveRandomNumbers.Add(randomNumber); //add the random number to the list
-                // fiveRandomNumber's capacity increases by 1
+                //fiveRandomNumber's capacity increases by 1
             }
-            /*
-            else
-            {
+            //
+            //else
+            //{
                //if the random number is already inside the list, regenerate a new random number
-            }
-            */
+            //}
         }
 
         //Debug.Log("fiveRandomNumbers.Capacity " + fiveRandomNumbers.Capacity);
-        /*foreach (int i in fiveRandomNumbers)
-        {
-            Debug.Log("Random number is " + i);
-        }
-        Debug.Log("\n");*/
+        //foreach (int i in fiveRandomNumbers)
+        //{
+        //    Debug.Log("Random number is " + i);
+        //}
+        //Debug.Log("\n");*/
 
         return fiveRandomNumbers;
     }
     public GameObject[] randomizeSpritePosition(GameObject spritePosition01, GameObject spritePosition02, GameObject spritePosition03, GameObject spritePosition04, GameObject spritePosition05)
     {
         //an array meant to contain all of the game object positions
-        GameObject[] spritePositions = new GameObject[5];
+        GameObject[]    spritePositions         = new GameObject[5];
 
         //selecting 5 unique random numbers between 0 and 4
-        List<int> fiveRandomNumbers = FiveUniqueRandomNumbers(5); 
-        int firstRandomIndex = fiveRandomNumbers[0]; //copying the first random number as the first random index
-        int secondRandomIndex = fiveRandomNumbers[1]; //copying the second random number as the second random index
-        int thirdRandomIndex = fiveRandomNumbers[2]; //copying the third random number as the third random index
-        int fourthRandomIndex = fiveRandomNumbers[3]; //copying the fourth random number as the fourth random index
-        int fifthRandomIndex = fiveRandomNumbers[4]; //copying the fifth random number as the fifth random index
+        List<int>       fiveRandomNumbers       = FiveUniqueRandomNumbers(5); 
+        int             firstRandomIndex        = fiveRandomNumbers[0]; //copying the first random number as the first random index
+        int             secondRandomIndex       = fiveRandomNumbers[1]; //copying the second random number as the second random index
+        int             thirdRandomIndex        = fiveRandomNumbers[2]; //copying the third random number as the third random index
+        int             fourthRandomIndex       = fiveRandomNumbers[3]; //copying the fourth random number as the fourth random index
+        int             fifthRandomIndex        = fiveRandomNumbers[4]; //copying the fifth random number as the fifth random index
 
         //assign the kana sounds randomly to the sprite positions
-        spritePositions[firstRandomIndex] = spritePosition01; //the first random index holds spritePosition01
-        spritePositions[secondRandomIndex] = spritePosition02; //the second random index holds spritePosition02
-        spritePositions[thirdRandomIndex] = spritePosition03; //the third random index holds spritePosition03
-        spritePositions[fourthRandomIndex] = spritePosition04; //the fourth random index holds spritePosition04
-        spritePositions[fifthRandomIndex] = spritePosition05; //the fifth random index holds spritePosition05
+        spritePositions[firstRandomIndex]       = spritePosition01; //the first random index holds spritePosition01
+        spritePositions[secondRandomIndex]      = spritePosition02; //the second random index holds spritePosition02
+        spritePositions[thirdRandomIndex]       = spritePosition03; //the third random index holds spritePosition03
+        spritePositions[fourthRandomIndex]      = spritePosition04; //the fourth random index holds spritePosition04
+        spritePositions[fifthRandomIndex]       = spritePosition05; //the fifth random index holds spritePosition05
 
         return spritePositions;
     }
-    public void ChangeSprite(List<Sprite> theList, int listIndex, GameObject spritePosition)
+    public void         ChangeSprite(List<Sprite> theList, int listIndex, GameObject spritePosition)
     {
-        SpriteRenderer oldSprite = spritePosition.GetComponent<SpriteRenderer>();
-        Sprite newSprite = theList[listIndex]; //chooses a random sprite from the array
+        SpriteRenderer  oldSprite               = spritePosition.GetComponent<SpriteRenderer>();
+        Sprite          newSprite               = theList[listIndex]; //chooses a random sprite from the array
+
+        //changing the old sprite to be the new sprite
         oldSprite.sprite = newSprite;
     }
-    public void ChangeSound(List<AudioClip> theList, int listIndex, GameObject spritePosition)
+    public void         ChangeSound(List<AudioClip> theList, int listIndex, GameObject spritePosition)
     {
-        SpriteRenderer oldSprite = spritePosition.GetComponent<SpriteRenderer>();
-        AudioClip newSound = theList[listIndex]; //chooses a random kana from the array
+        SpriteRenderer  oldSprite               = spritePosition.GetComponent<SpriteRenderer>();
+        AudioClip       newSound                = theList[listIndex]; //chooses a random kana from the array
+
+        //changing the old sound to be the new sounds
         oldSprite.GetComponent<AudioSource>().clip = newSound;
     }
-    public void Spawn() //uploads 5 random kana and their sounds to be shown in the game 
+    public void         Spawn() //uploads 5 random kana and their sounds to be shown in the game 
     {
         //chooses 5 random numbers between 0 and X (including the last index of kanaList)...(i.e., choosing 5 random kana and their sounds from the list)
-        List<int> fiveRandomNumbers = FiveUniqueRandomNumbers(kanaList.Capacity);
-        int firstRandomIndex = fiveRandomNumbers[0];
-        int secondRandomIndex = fiveRandomNumbers[1];
-        int thirdRandomIndex = fiveRandomNumbers[2];
-        int fourthRandomIndex = fiveRandomNumbers[3];
-        int fifthRandomIndex = fiveRandomNumbers[4];
+        List<int>       fiveRandomNumbers       = FiveUniqueRandomNumbers(kanaList.Capacity);
+        int             firstRandomIndex        = fiveRandomNumbers[0];
+        int             secondRandomIndex       = fiveRandomNumbers[1];
+        int             thirdRandomIndex        = fiveRandomNumbers[2];
+        int             fourthRandomIndex       = fiveRandomNumbers[3];
+        int             fifthRandomIndex        = fiveRandomNumbers[4];
 
         //spritePosition has all of the kana sprite locations, and randomly choose where the new kana sprite is going to be positioned
-        GameObject[] randomSpritePosition = randomizeSpritePosition(kanaPosition01, kanaPosition02, kanaPosition03, kanaPosition04, kanaPosition05);
+        GameObject[]    randomSpritePosition    = randomizeSpritePosition(kanaPosition01, kanaPosition02, kanaPosition03, kanaPosition04, kanaPosition05);
 
         //uploading the new kana sprites to Unity
-        ChangeSprite(kanaList, firstRandomIndex, randomSpritePosition[0]);
+        ChangeSprite(kanaList, firstRandomIndex,  randomSpritePosition[0]);
         ChangeSprite(kanaList, secondRandomIndex, randomSpritePosition[1]);
-        ChangeSprite(kanaList, thirdRandomIndex, randomSpritePosition[2]);
+        ChangeSprite(kanaList, thirdRandomIndex,  randomSpritePosition[2]);
         ChangeSprite(kanaList, fourthRandomIndex, randomSpritePosition[3]);
-        ChangeSprite(kanaList, fifthRandomIndex, randomSpritePosition[4]);
+        ChangeSprite(kanaList, fifthRandomIndex,  randomSpritePosition[4]);
 
         //spritePosition has all of the sound sprite locations
         randomSpritePosition = randomizeSpritePosition(soundPosition01, soundPosition02, soundPosition03, soundPosition04, soundPosition05);
 
         //uploading the new kana sounds to Unity
-        ChangeSound(soundList, firstRandomIndex, randomSpritePosition[0]);
+        ChangeSound(soundList, firstRandomIndex,  randomSpritePosition[0]);
         ChangeSound(soundList, secondRandomIndex, randomSpritePosition[1]);
-        ChangeSound(soundList, thirdRandomIndex, randomSpritePosition[2]);
+        ChangeSound(soundList, thirdRandomIndex,  randomSpritePosition[2]);
         ChangeSound(soundList, fourthRandomIndex, randomSpritePosition[3]);
-        ChangeSound(soundList, fifthRandomIndex, randomSpritePosition[4]);
+        ChangeSound(soundList, fifthRandomIndex,  randomSpritePosition[4]);
     }
-    void Refresh()
+    void                Refresh()
     {
         //make sure all of the sprites are not hiding and are visible
         kanaPosition01.SetActive(true);
@@ -142,16 +145,41 @@ public class List_Of_Kana : MonoBehaviour
         soundPosition04.transform.position  =   new(3.60f,  -2.41f, 0.00f);
         soundPosition05.transform.position  =   new(6.98f,  -2.41f, 0.00f);
     }
-    void Start() //called before the first frame update
+    void                Start() //called before the first frame update
     {
         Spawn();
+
     }
-    void Update() //called once per frame
+    void                Update() //called once per frame
     {
+        //removing the used sprite+sound from the pool of sounds+sprites
+        Sprite usedKana1 = kanaPosition01.GetComponent<SpriteRenderer>().sprite;
+        kanaList.Remove(usedKana1);
+        Sprite usedKana2 = kanaPosition02.GetComponent<SpriteRenderer>().sprite;
+        kanaList.Remove(usedKana2);
+        Sprite usedKana3 = kanaPosition03.GetComponent<SpriteRenderer>().sprite;
+        kanaList.Remove(usedKana3);
+        Sprite usedKana4 = kanaPosition04.GetComponent<SpriteRenderer>().sprite;
+        kanaList.Remove(usedKana4);
+        Sprite usedKana5 = kanaPosition05.GetComponent<SpriteRenderer>().sprite;
+        kanaList.Remove(usedKana5);
+        AudioClip usedSound1 = soundPosition01.GetComponent<AudioSource>().clip;
+        soundList.Remove(usedSound1);
+        AudioClip usedSound2 = soundPosition02.GetComponent<AudioSource>().clip;
+        soundList.Remove(usedSound2);
+        AudioClip usedSound3 = soundPosition03.GetComponent<AudioSource>().clip;
+        soundList.Remove(usedSound3);
+        AudioClip usedSound4 = soundPosition04.GetComponent<AudioSource>().clip;
+        soundList.Remove(usedSound4);
+        AudioClip usedSound5 = soundPosition05.GetComponent<AudioSource>().clip;
+        soundList.Remove(usedSound5);
+
         if (!(kanaPosition01.activeSelf) && !(kanaPosition02.activeSelf) && !(kanaPosition03.activeSelf) && !(kanaPosition04.activeSelf) && !(kanaPosition05.activeSelf) && !(soundPosition01.activeSelf) && !(soundPosition02.activeSelf) && !(soundPosition03.activeSelf) && !(soundPosition04.activeSelf) && !(soundPosition05.activeSelf))
         {
             //Debug.Log("Time to respawn!");
+
             Refresh(); //makes sure all of the sprites are visible and they're back in their original places
+
             Spawn();
         }
     }
